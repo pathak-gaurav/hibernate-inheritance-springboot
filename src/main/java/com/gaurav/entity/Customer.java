27837fe42921010fa0ac726a0c3b5a882ad4ad85
gauravpathak <gauravpathak.spring@gmail.com>
2019-02-24 21:46:37 +0530

    Single Table with Inheritance - By Default it is always Single Table even without inheritance

--- src/main/java/com/gaurav/entity/Customer.java
+++ src/main/java/com/gaurav/entity/Customer.java
@@ -4,16 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serializable;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 public class Customer implements Serializable {
 
     @Id
